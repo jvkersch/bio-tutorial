2d5f7066eabd841a89107d5735ad026da99af58e
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2018-08-22 21:49:37 +0100

    MAINT: Update notebooks

--- deploy/setup_env.sh
+++ deploy/setup_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
-PKGS="biopython scikit_bio jupyter reportlab pillow"
+PKGS="biopython scikit_bio jupyter reportlab pillow pyqt"
 
 edm envs create --force bio-tutorial --version 3.6
 edm install -e bio-tutorial $PKGS -y
 
-edm run -e bio-tutorial pip install nglview
+edm run -e bio-tutorial -- pip install nglview ete3
 
 jupyter-nbextension enable nglview --py --sys-prefix
